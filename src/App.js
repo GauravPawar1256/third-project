24bb067ef61dc7eb11c127f88a69e7144dea3f9f
gaurav.pawar <pawargaurav1256@gmail.com>
2025-08-23 14:44:09 +0530

    display object on UI

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+
+ function App() {
+  const student={Name:"gaurav",Standard:"TYBCS",Collage:"B.J.Collage"};
+  return (
+    <div>
+      <p>
+        <h1>Name:{student.Name}</h1>
+        <h2>Standard:{student.Standard}</h2>
+        <h3>Collage:{student.Collage}</h3>
+      </p>
+
+    </div>
+    );
+}
+
+export default App;
