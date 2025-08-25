45be15fae3c20aa0e85100daf19eb6354e956d61
gaurav.pawar <pawargaurav1256@gmail.com>
2025-08-25 16:30:58 +0530

    created new component and called at it app.js

--- src/App.js
+++ src/App.js
@@ -1,35 +1,10 @@
 
 import './App.css';
+import Greetings from './Greetings';
 function App() {
-  let student = [{ Sr_NO: "1", Name: "Gaurav", Marks: "100" },
-  { Sr_NO: "2", Name: "Shardul", Marks: "90" },
-  { Sr_NO: "3", Name: "Shravani", Marks: "100" },
-  { Sr_NO: "4", Name: "Trusha", Marks: "70" },
-  { Sr_NO: "5", Name: "Rutuja", Marks: "100" },
-  { Sr_NO: "6", Name: "Ayush", Marks: "50" }];
+
   return (
-    <div>
-
-      <table border={1}>
-        <tr>
-          <th>Sr.No.</th>
-          <th>Name</th>
-          <th>Marks</th>
-        </tr>
-         { student.map((singleElement)=>{
-          return <tr className= {singleElement.Marks==100 && "color"}>
-         
-            <td>{singleElement.Sr_NO}</td>
-            <td>{singleElement.Name}</td>
-            <td>{singleElement.Marks}</td>
-          </tr>
-
-        })}
-
-      </table>
-
-
-    </div>
+    <Greetings/>
   );
 }
 
---
+++ src/Greetings.js
@@ -0,0 +1,8 @@
+function Greetings(){
+
+
+return(
+    <h1>hello world</h1>
+);
+}
+export default Greetings;
