2351d630a1415fe19f8ec279bc7bb5c9cde53e70
gaurav.pawar <pawargaurav1256@gmail.com>
2025-08-25 15:28:41 +0530

    arrray of an object in a table

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 
 
  function App() {
-  const student={Name:"gaurav",Standard:"TYBCS",Collage:"B.J.Collage"};
+  let student=[{Sr_NO:"1",Name:"Gaurav",Marks:"100"},{Sr_NO:"2",Name:"Shardul",Marks:"90"},
+    {Sr_NO:"3",Name:"Shravani",Marks:"80"},
+    {Sr_NO:"4",Name:"Trusha",Marks:"70"},{Sr_NO:"5",Name:"Rutuja",Marks:"60"},
+  {Sr_NO:"6",Name:"Ayush",Marks:"50"}];
   return (
     <div>
-      <p>
-        <h1>Name:{student.Name}</h1>
-        <h2>Standard:{student.Standard}</h2>
-        <h3>Collage:{student.Collage}</h3>
-      </p>
+      
+        <table border={1}>
+          <tr>
+            <th>Sr.No.</th>
+            <th>Name</th>
+            <th>Marks</th>
+          </tr>
+          {student.map((singleElement)=>
+            <tr>
+              <td>{singleElement.Sr_NO}</td>
+              <td>{singleElement.Name}</td>
+              <td>{singleElement.Marks}</td>
+            </tr>
+
+          )}
+        
+        </table>
+      
 
     </div>
     );
